ae1e98650da19cdde5893bb7124a81c4f48d33a1
ivishalanand <vishal.ananad456@gmail.com>
2022-01-19 14:45:08 +0530

    Added audio book generation screen

--- static/search/script.js
+++ static/search/script.js
@@ -5,7 +5,6 @@ document.getElementById("next-click").addEventListener("click", function () {
     var nextPage = parseInt(document.getElementById("page-number").innerHTML) + 1
     var nextPageQuery = queryPartial.concat(nextPage)
     document.getElementById("next-click").value = nextPageQuery
-    console.log(document.getElementById("next-click").value)
     form.submit();
 });
 
@@ -14,10 +13,16 @@ document.getElementById("prev-click").addEventListener("click", function () {
     var nextPage = parseInt(document.getElementById("page-number").innerHTML) - 1
     var nextPageQuery = queryPartial.concat(nextPage)
     document.getElementById("prev-click").value = nextPageQuery
-    console.log(document.getElementById("prev-click").value)
     form.submit();
 });
 
 if (parseInt(document.getElementById("page-number").innerHTML)==1) {
     document.getElementById("prev-click").style.display = 'none'
   }
+
+
+function generateAudiobook(query) {
+  document.getElementById(query).submit();
+  document.querySelectorAll('.overlay')[0].style.display ="flex"
+}
+
