fcc14af70d011f63f4afc2c80e26f5e62592038e
ivishalanand <vishal.ananad456@gmail.com>
2022-02-19 22:42:37 +0530

    Fixed book loading on first page

--- bookaudifier/settings.py
+++ bookaudifier/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,9 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
 '/var/www/static/',
 ]
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- bookaudifier/urls.py
+++ bookaudifier/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('search.urls'))
+    path('', include('search.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
